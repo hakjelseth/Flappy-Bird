45c9ac033021d92daa13f939ed3db93fa7da4388
hakjelseth <hakjelse@student.uio.no>
2021-07-21 12:44:58 +0200

    init commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    public GameObject ScoreCanvas;
+    public scoreBoardText scoreboard;
+    public bool restart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        gameOverCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        ScoreCanvas.SetActive(false);
+        scoreboard.updateText();
+        Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/scoreBoardText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class scoreBoardText : MonoBehaviour
+{
+
+   public Text currentScore;
+   public Text highScore;
+
+   public GameObject bronzeMedal;
+   public GameObject silverMedal;
+   public GameObject goldMedal;
+
+   
+   public void updateText()
+   {
+      if (Score.score > 10)
+      {
+         silverMedal.SetActive(true);
+      }
+      else if (Score.score > 20)
+      {
+         goldMedal.SetActive(true);
+      }
+      else
+      {
+         bronzeMedal.SetActive(true);
+      }
+
+      if (Score.score > Score.highScore)
+      {
+         SaveSystem.SaveGame(Score.score);
+         Score.highScore = Score.score;
+      }
+      
+      currentScore.text = Score.score.ToString();
+      highScore.text = Score.highScore.ToString();
+
+   }
+}
